2f2c2042b605fb0b092ba1d1fb230e31ae2866a7
Shawn Hoerner <shoerner+git@shoerner.net>
2018-09-18 22:40:45 -0400

    Use const for port assignment

--- src/Server.tsx
+++ src/Server.tsx
@@ -3,8 +3,9 @@ import express from 'express';
 import path from 'path';
 import { renderToString } from 'react-dom/server';
 import { BaseLayout } from './BaseLayout';
-import { StyleSheet, StyleSheetServer } from 'aphrodite/no-important';
+import { StyleSheetServer } from 'aphrodite/no-important';
 
+const APP_ASSIGNED_PORT = 2048;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "../dist")));
@@ -14,8 +15,8 @@ app.get("/*", (_request, response) => {
     response.end(htmlTemplate(<BaseLayout />));
 });
 
-app.listen(2048);
-console.log("Server started on 2048");
+app.listen(APP_ASSIGNED_PORT);
+console.log("Server started on", APP_ASSIGNED_PORT);
 
 function htmlTemplate(reactDom: React.ReactElement<any>) {
     const { html, css } = StyleSheetServer.renderStatic(() => renderToString(reactDom));
