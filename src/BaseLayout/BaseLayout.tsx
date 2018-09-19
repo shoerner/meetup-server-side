27d1ddda008f99b9d25a8f56236af4880de8df85
Shawn Hoerner <shoerner+git@shoerner.net>
2018-09-18 22:01:41 -0400

    Adds lifecycle information for testing

--- src/BaseLayout/BaseLayout.tsx
+++ src/BaseLayout/BaseLayout.tsx
@@ -2,8 +2,54 @@ import * as React from 'react';
 import { css } from 'aphrodite';
 import { styles } from './BaseLayout.styles';
 
-export const BaseLayout = () => {
-    return (
-        <h1 className={css(styles.header)}>This was server rendered</h1>
-    );
+export class BaseLayout extends React.Component<{}>{
+    static getDerivedStateFromProps(_props: any, _state: any) {
+        console.log("deriving state");
+        return null;
+    }
+
+    public getSnapshotBeforeUpdate() {
+        console.log("snapshot!");
+        return null;
+    }
+
+    public componentDidCatch(_error: any, _info: any) {
+        console.log("ComponentDidCatch");
+    }
+
+    public componentDidUpdate() {
+        console.log("ComponentDidUpdate");
+    }
+
+    public componentDidMount() {
+        console.log("ComponentDidMount");
+    }
+
+    public componentWillReceiveProps(nextProps: {}) {
+        console.log("componentWillReceiveProps");
+    }
+
+    public shouldComponentUpdate() {
+        console.log("shouldComponentUpdate")
+        return true;
+    }
+
+    public componentWillMount() {
+        console.log("ComponentWillMount");
+    }
+
+    public componentWillUnmount() {
+        console.log("componentWillUnmount");
+    }
+
+    public componentWillUpdate() {
+        console.log("componentWillUpdate");
+    }
+
+    public render() {
+        console.log("rendering!");
+        return (
+            <h1 className={css(styles.header)}>This was server rendered</h1>
+        );
+    }
 }
